821bca73a77b552182dd171b7e906380beff0083
george <naifonovgk@gmail.com>
2021-06-04 10:31:26 +0300

    Fix: removes unnecessary variable, inlines array

--- project/src/components/pointsList/pointsList.jsx
+++ project/src/components/pointsList/pointsList.jsx
@@ -5,11 +5,10 @@ import Point from '../point/point';
 function PointsList(props) {
   const {pointsAmount} = props;
 
-  const points = new Array(pointsAmount).fill().map(((value, index) => <Point key={index}/>));
-
   return (
     <div className="cities__places-list places__list tabs__content">
-      {points}
+      {/* eslint-disable-next-line react/no-array-index-key */}
+      {new Array(pointsAmount).fill().map(((value, index) => <Point key={index}/>))}
     </div>
   );
 }
