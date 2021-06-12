e0eed108f6cdc52090f306f4052903a9d8408eb2
george <naifonovgk@gmail.com>
2021-06-12 23:48:27 +0300

    Feat: ads routing to point-info-page

--- project/src/components/point/point.jsx
+++ project/src/components/point/point.jsx
@@ -2,9 +2,12 @@ import React, {useState} from 'react';
 import offerProp from '../../props/offer.prop';
 import pointTypeProp from '../../props/pointType.prop';
 import BookmarkButton from '../bookmark-button/bookmark-button';
+import {generatePath, Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
 
 function Point(props) {
   const {offer, type} = props;
+  const {isPremium, isFavorite, rating, price, previewImage, id, title} = offer;
 
   // eslint-disable-next-line no-unused-vars
   const [hoveredOffer, setHoveredOffer] = useState([]);
@@ -14,34 +17,33 @@ function Point(props) {
       className={type.articleClass}
       onMouseEnter={() => setHoveredOffer(offer)}
     >
-      {offer.isPremium && (
+      {isPremium && (
         <div className="place-card__mark">
           <span>Premium</span>
         </div>
       )}
       <div className={type.imageContainerClass}>
-        <a href="#">
-          <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place image"/>
-        </a>
+        <Link to={{pathname: generatePath(AppRoute.ROOM, {id})}}>
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image"/>
+        </Link>
       </div>
       <div className={type.infoContainerClass}>
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{offer.price}</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <BookmarkButton isFavorite={offer.isFavorite}/>
+          <BookmarkButton isFavorite={isFavorite}/>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: (`${offer.rating * 20}%`)}}/>
+            <span style={{width: (`${rating * 20}%`)}}/>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{offer.title}</a>
+          <Link to={{pathname: generatePath(AppRoute.ROOM, {id})}}>{title}</Link>
         </h2>
-        <p className="place-card__type">{offer.type}</p>
       </div>
     </article>
   );
